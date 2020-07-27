47389784de8b34162ea4c2418e3ab5364153d10e
KazutoshiYamaguchi <umaibou300pon@icloud.com>
2020-07-27 16:57:52 +0900

    StepAsideUnityChan 3DゲームアプリVol.2
    ユニティちゃんが通り過ぎて画面外に出たアイテム破棄しました。

---
+++ Assets/Scripts/ItemDestroyer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroyer : MonoBehaviour {
+
+
+
+	private GameObject carpre;
+	private GameObject conepre;
+	private GameObject coinpre;
+    private GameObject unitychan;
+	// Use this for initialization
+	void Start () {
+		
+		this.unitychan = GameObject.Find("unitychan");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		this.conepre = GameObject.Find("TrafficConePrefab(Clone)");
+		this.carpre = GameObject.Find("CarPrefab(Clone)");
+		this.coinpre = GameObject.Find("CoinPrefab(Clone)");
+
+		if (conepre)
+		{
+			if (this.conepre.transform.position.z < unitychan.transform.position.z)
+			{
+				Destroy(this.conepre.gameObject);
+			}
+        }
+        else
+        {
+			Debug.Log("No ConePrefab");
+		}
+
+		if (carpre)
+		{
+			if (this.carpre.transform.position.z < unitychan.transform.position.z)
+			{
+				Destroy(this.carpre.gameObject);
+			}
+		}
+        else
+        {
+			Debug.Log("No CarPrefab");
+		}
+
+		if (coinpre)
+		{
+			if (this.coinpre.transform.position.z < unitychan.transform.position.z)
+			{
+				Destroy(this.coinpre.gameObject);
+			}
+		}
+        else
+        {
+			Debug.Log("No CoinPrefab");
+		}
+		
+
+	}
+}
